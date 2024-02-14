97c00dd9dfca9d48f557990eb2e8c8ba0e3bc40f
Benni Setiady <bennysetiady111@gmail.com>
2024-02-14 21:31:55 +0700

    kelima

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = 'angular-with-dinda';
+  title = 'hello';
 
   ngOnInit(): void {
     // this.playAudio();
